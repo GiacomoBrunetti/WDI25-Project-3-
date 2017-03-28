6625eac741037a6dffbedbd71aacddbf87aae7c6
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2017-03-28 14:08:13 +0100

    navbar as concept

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,19 @@
 angular
   .module('hrApp', ['ui.router', 'ngResource', 'satellizer', 'ngMessages']);
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const nav =   document.getElementsByTagName('nav')[0];
+//   function showNav() {
+//     if(document.URL==='localhost:7000') {
+//       nav.className += 'hidden';
+//     } else {
+//       nav.classList.remove('hidden');
+//     }
+//
+//   }
+//
+//   showNav();
+//
+// });
