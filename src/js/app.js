d3e589c15def449cc975e6cae9c8f7ae1bc16984
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2017-03-29 11:52:46 +0100

    styled edit form

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,11 @@ angular
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const labels = document.getElementsByTagName('label');
-  console.log(labels);
-  for (let i = 0; i < labels.length; i++) {
-    labels[i].addEventListener('click', () => {
-      console.log('click');
-      labels[i].className +='selected';
-    });
-  }
+  const chooseFile = document.getElementById('chooseFile');
+  console.log(chooseFile);
+  chooseFile.addEventListener('change', () => {
+    chooseFile.innerText = this.value;
+  });
+
 
 });
